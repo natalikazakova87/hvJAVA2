c05594a8f0f7376570c3d2bdff19770a42908a23
natalikazakova <natalikazakova87@gmail.com>
2023-06-04 22:02:10 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("привет путешественник");
+
+        int amount = 69_458;
+        int oneMile = 20;
+
+        int miles = amount / oneMile;
+        System.out.println("мили: " + miles);
+        System.out.println("Ваши мили начислены");
+    }
+}
